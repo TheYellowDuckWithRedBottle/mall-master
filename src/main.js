bcdef769324bced38645f2b2c3a9454c7f5e12cf
1521681359@qq.com <1521681359@qq.com>
2020-10-08 16:58:12 +0800

    使用fastclick移动端300毫秒延迟

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import router from './router/index'
 import store from './store'
 
 import toast from 'components/common/Toast'
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 //安装toast插件
 Vue.use(toast)//会调用这个对象的install
 new Vue({
